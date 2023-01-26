930375ffacd1ca81e001b57d47b6824578452528
Jacetice <jace.crowe@gmail.com>
2023-01-25 19:35:09 -0600

    full react router set up, not coordinated with projects yet

---
+++ client/App.jsx
@@ -0,0 +1,198 @@
+
+/**
+ * **************************************************
+ *
+ * @module App
+ *
+ * @description
+ * App.tsx is the main React component for the App.
+ * This is the starting point for the CodersCorner SPA.
+ *
+ * **************************************************
+ */
+
+import { useEffect } from 'react';
+// ReactRouter v. 6.4
+import {
+  //Provide our base page
+  Outlet,
+  //Clarify which route we are on
+  NavLink,
+  //Use instead of external links, navigates in browser and stops reloading of the dom
+  Link,
+  //This allows us to update the UI without needing useState, onSubmit, and useEffect. Automatically syncs with data
+  useLoaderData,
+  //We use form because with client side apps, if we just link, it will change the request method
+  Form,
+  redirect,
+  //Used to add global pedning UI
+  useNavigation,
+  //To make the filtering happen on every keystroke
+  useSubmit
+} from 'react-router-dom';
+//RR Tutorial infromation
+import { getContacts, createContact } from './contacts'
+
+export const WIDGET_SIZE = 5
+
+//RR Tutorial infromation
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
+};
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+
+const App = () => {
+  const { contacts, q } = useLoaderData();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
+
+  //! State
+  //Doing this is the same as using a controlled component in React state, it's one point instead of three.
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+
+  
+  return (
+      <>
+      <div id='sidebar'>
+        <h1>Projects</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
+              id="q"
+              className={searching ? "loading" : ""}
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+              defaultValue={q}
+              onChange={(e) => {
+                //to prevent every letter from appearing in the browser history stack
+                const isFirstSearch = q == null;
+                submit(e.currentTarget.form, {
+                  replace: !isFirstSearch,
+                })
+              }}
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={!searching}
+            />
+            <div className="sr-only" aria-live="polite"></div>
+          </Form>
+          <Form method="post">
+            <button type='submit'>New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    //this is how we pass the information along
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+          <ul>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div
+        id='detail'
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
+        <Outlet />
+      </div>
+      </>
+    );
+}
+
+export default App;
+
+
+
+//! Class App Component !//
+// const mapStateToProps = (store) => ({
+// });
+// const mapDispatchToProps = (dispatch) => ({
+// });
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//   }
+//   // Initialize all current curriculum and YAML content on mount.
+//   componentDidMount() {
+//   }
+//   componentDidUpdate(prevProps) {
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <StarRating />
+//         <Widget />
+//         {/* < HelloWorld / > */}
+//       </div>
+//     );
+//   }
+// }
+// {/* export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App)); */}
+// export default App;
--- client/index.tsx
+++ client/index.tsx
@@ -7,18 +7,37 @@
  * ************************************
  */
 
-import React from 'react';
+import React, { Children } from 'react';
 import { createRoot } from 'react-dom/client';
 // State Management
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./App";
+
 
 // React entry-point component
 import App from './App';
+
+// React Router Information
+import ErrorPage from './components/ErrorPage';
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact';
+import EditContact, {
+  action as editAction,
+} from './routes/edit';
+import {
+  action as destroyAction
+} from './routes/destroy';
+import Index from './routes/index';
+
 // Redux state management
 import store from './store/index'
 
@@ -29,7 +48,38 @@ import './stylesheets/global.css'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            //Tutorial is being lazy by using same loader, should use different
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+          }
+        ]
+      }
+    ]
   },
 ]);
 
@@ -49,3 +99,41 @@ const render = (Component) => {
 };
 
 render(App);
+
+//? Optional JSX Alternative to Routerfiles
+// import {
+//   createRoutesFromElements,
+//   createBrowserRouter,
+// } from "react-router-dom";
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route
+//       path="/"
+//       element={<Root />}
+//       loader={rootLoader}
+//       action={rootAction}
+//       errorElement={<ErrorPage />}
+//     >
+//       <Route errorElement={<ErrorPage />}>
+//         <Route index element={<Index />} />
+//         <Route
+//           path="contacts/:contactId"
+//           element={<Contact />}
+//           loader={contactLoader}
+//           action={contactAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/edit"
+//           element={<EditContact />}
+//           loader={contactLoader}
+//           action={editAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/destroy"
+//           action={destroyAction}
+//         />
+//       </Route>
+//     </Route>
+//   )
+// );
